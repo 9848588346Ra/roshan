7a2aa4aebdb48557918a0e8323178e29054523b6
Swastika Khadka <swastee@Swastikas-MacBook-Pro.local>
2024-06-26 10:10:36 +0545

    workflows

--- app/src/main/java/com/example/roomease/CarRental.kt
+++ app/src/main/java/com/example/roomease/CarRental.kt
@@ -1,32 +1,17 @@
 package com.example.roomease
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    lateinit var dataBinding: ActivityMainBinding
-
+class CarRental : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        dataBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(dataBinding.root)
-
-        dataBinding.background
-        dataBinding.logo
-        dataBinding.username
-        dataBinding.Forgetpassword
-        dataBinding.password
-        dataBinding.butlogin
-        dataBinding.butsignup
-
+        setContentView(R.layout.activity_car_rental)
+ 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
